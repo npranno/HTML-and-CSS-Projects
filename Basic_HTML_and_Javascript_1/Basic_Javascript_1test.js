b40430fd459c49597081afc55e9c786da71a8afd
npranno <77467876+npranno@users.noreply.github.com>
2021-01-19 12:07:17 -0800

    Basic HTML and Java
    Added to Assignment2test2.html and created basic HTML and my first JavaScript function

---
+++ Basic_HTML_and_Javascript_1/Basic_Javascript_1test.js
@@ -0,0 +1,7 @@
+alert("This is a test.")
+function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
